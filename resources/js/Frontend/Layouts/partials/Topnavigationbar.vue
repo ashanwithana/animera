123f7154d4c9e2004b0a30fdb5b1db5b29de22bb
Kalindu <kalindugayanjith@gmail.com>
2025-08-20 22:24:57 +0530

    nav bar design fixed

--- resources/js/Frontend/Layouts/partials/Topnavigationbar.vue
+++ resources/js/Frontend/Layouts/partials/Topnavigationbar.vue
@@ -1,29 +1,15 @@
 <template>
   <nav class="navbar position-fixed w-100">
-    <div class="container-fluid position-relative">
-      <a class="navbar-brand" href="#">
-        <img src="images/animera-logo.png" alt="Bootstrap" width="90">
-      </a>
-      <div class="login-btn-wrapper">
-        <div class="position-absolute" style="top: 10px; right:20px;">
-          <div v-if="user == 1">
-            <Link type="button" class="text-black" :href="route('login')">Login <i
-              class="fa-solid fa-right-to-bracket ps-1"></i></Link>
-          </div>
-          <div v-else>
-            <Link type="button" class="text-black">Logout <i class="fa-solid fa-arrow-right-from-bracket ps-1"></i></Link>
-          </div>
-        </div>
-      </div>
+    <div class="container-fluid nav-container">
+      <Link :href="route('index')" class="navbar-brand">
+        <img src="images/animera-logo.png" alt="animera-logo" width="90">
+      </Link>
     </div>
   </nav>
 </template>
 
 <script>
 import { Link } from '@inertiajs/inertia-vue3';
-
-
-
 
 export default {
   components: {
